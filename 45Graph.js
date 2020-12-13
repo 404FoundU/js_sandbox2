c3d46ec9dfefc6ca2bab6edc695d32462178c43e
upillai <upillai@xpressdocs.com>
2020-12-13 10:09:26 -0600

    Added graph

---
+++ 45Graph.js
@@ -0,0 +1,29 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(v1, v2) {
+        // this.addVertex(v1);
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+
+    removeEdge(v1,v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v1].filter(v => v !== v1);
+    }
+
+    removeVertex(v1) {
+        for (let vertex of this.adjacencyList[v1]) {
+            this.removeEdge(vertex, v1);
+        }
+        delete this.adjacencyList[v1];
+    }
+}
