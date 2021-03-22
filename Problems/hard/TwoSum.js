b22a268143ab0870efbd4db17bcd884565ca1069
upillai <upillai@xpressdocs.com>
2021-03-22 12:21:27 -0500

    Added TwoSum modified

--- Problems/hard/TwoSum.js
+++ Problems/hard/TwoSum.js
@@ -2,19 +2,14 @@
 
 const twoSum = (arr, target) => {
     const leftOver = {};
-    let indices = [];
     for (let i = 0; i < arr.length; i++) {
-        let diff = target - arr[i];
-        if (diff > 0) {
-            if (arr[i] in leftOver) {
-                indices =  [leftOver[arr[i]], i];
-                break;
+        let current = arr[i];
+        let diff = target - current;
+            if (current in leftOver) {
+                return [current, arr[leftOver[current]]]
             }
             leftOver[diff] = i;
-        }
+
     }
-    return [arr[indices[0]], arr[indices[1]]];
-
-
 };
-console.log(twoSum([1, 2, 3, 9, 6], 8));
+console.log(twoSum([1, 2, 3, 9, 6], 15));
