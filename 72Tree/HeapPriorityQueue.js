24853ab3972cdef17e18d5bfd99e9d544c4c9f1c
upillai <upillai@xpressdocs.com>
2021-04-07 11:48:45 -0500

    added kClosestPoints

--- 72Tree/HeapPriorityQueue.js
+++ 72Tree/HeapPriorityQueue.js
@@ -1,6 +1,7 @@
 class Node {
-    constructor(val) {
+    constructor(val,priority) {
         this.val = val;
+        this.priority = priority;
     }
 }
 
@@ -9,8 +10,8 @@ class PriorityQueue {
         this.values = [];
     }
 
-    enqueue(val) {
-        let node = new Node(val);
+    enqueue(val,priority) {
+        let node = new Node(val,priority);
         this.values.push(node);
         if (this.values.length > 1) {
             this.bubbleUp();
@@ -23,7 +24,7 @@ class PriorityQueue {
         while (idx > 0) {
             let parentIdx = Math.floor((idx - 1) / 2);
             let parent = this.values[parentIdx];
-            if (parent.val >= el.val) {
+            if (parent.priority >= el.priority) {
                 break;
             }
             this.values[parentIdx] = el;
@@ -52,8 +53,8 @@ class PriorityQueue {
         let length = this.values.length;
         while (idx < length) {
             let el = this.values[idx];
-            let leftChild = new Node(0);
-            let rightChild = new Node(0);
+            let leftChild = new Node(0,0);
+            let rightChild = new Node(0,0);
             let leftChildIdx = (idx * 2) + 1;
             let rightChildIdx = (idx * 2) + 2;
             if (leftChildIdx < length) {
@@ -62,13 +63,13 @@ class PriorityQueue {
             if (rightChildIdx < length) {
                 rightChild = this.values[rightChildIdx];
             }
-            if (leftChild.val > el.val && leftChild.val > rightChild.val) {
+            if (leftChild.priority > el.priority && leftChild.priority > rightChild.priority) {
                 this.values[leftChildIdx] = el;
                 this.values[idx] = leftChild;
                 idx = leftChildIdx;
                 continue;
             }
-            if (rightChild.val > el.val && rightChild.val > leftChild.val) {
+            if (rightChild.priority > el.priority && rightChild.priority > leftChild.priority) {
                 this.values[rightChildIdx] = el;
                 this.values[idx] = rightChild;
                 idx = rightChildIdx;
@@ -80,14 +81,14 @@ class PriorityQueue {
 }
 
 let pQ = new PriorityQueue();
-pQ.enqueue(39);
-pQ.enqueue(18);
-pQ.enqueue(55);
-pQ.enqueue(41);
-pQ.enqueue(12);
-pQ.enqueue(33);
-pQ.enqueue(27);
-pQ.enqueue(38);
-pQ.extractMax();
+pQ.enqueue(39,3);
+pQ.enqueue(18, 4);
+pQ.enqueue(55, 1);
+pQ.enqueue(41, 4);
+pQ.enqueue(12, 9);
+pQ.enqueue(33, 11);
+pQ.enqueue(27, 8);
+pQ.enqueue(38, 7);
+console.log(pQ.extractMax());
 console.log(pQ.values);
 
--- 72Tree/kclosestPoints.js
+++ 72Tree/kclosestPoints.js
@@ -1,6 +1,10 @@
+// https://leetcode.com/problems/k-closest-points-to-origin/
+
+
 class Node {
-    constructor(val) {
+    constructor(val,priority) {
         this.val = val;
+        this.priority = priority;
     }
 }
 
@@ -9,8 +13,8 @@ class PriorityQueue {
         this.values = [];
     }
 
-    enqueue(val) {
-        let node = new Node(val);
+    enqueue(val,priority) {
+        let node = new Node(val,priority);
         this.values.push(node);
         if (this.values.length > 1) {
             this.bubbleUp();
@@ -23,7 +27,7 @@ class PriorityQueue {
         while (idx > 0) {
             let parentIdx = Math.floor((idx - 1) / 2);
             let parent = this.values[parentIdx];
-            if (parent.val >= el.val) {
+            if (parent.priority <= el.priority) {
                 break;
             }
             this.values[parentIdx] = el;
@@ -35,7 +39,7 @@ class PriorityQueue {
     extractMax() {
         let max = null;
         if (this.values.length > 0) {
-             max = this.values[0];
+            max = this.values[0];
             let el = this.values.pop();
             this.values[0] = el;
             if (this.values.length > 0) {
@@ -52,8 +56,8 @@ class PriorityQueue {
         let length = this.values.length;
         while (idx < length) {
             let el = this.values[idx];
-            let leftChild = new Node(0);
-            let rightChild = new Node(0);
+            let leftChild = new Node([],1000);
+            let rightChild = new Node([],1000);
             let leftChildIdx = (idx * 2) + 1;
             let rightChildIdx = (idx * 2) + 2;
             if (leftChildIdx < length) {
@@ -62,13 +66,13 @@ class PriorityQueue {
             if (rightChildIdx < length) {
                 rightChild = this.values[rightChildIdx];
             }
-            if (leftChild.val > el.val && leftChild.val > rightChild.val) {
+            if (leftChild.priority < el.priority && leftChild.priority < rightChild.priority) {
                 this.values[leftChildIdx] = el;
                 this.values[idx] = leftChild;
                 idx = leftChildIdx;
                 continue;
             }
-            if (rightChild.val > el.val && rightChild.val > leftChild.val) {
+            if (rightChild.priority < el.priority && rightChild.priority < leftChild.priority) {
                 this.values[rightChildIdx] = el;
                 this.values[idx] = rightChild;
                 idx = rightChildIdx;
@@ -79,15 +83,34 @@ class PriorityQueue {
     }
 }
 
-let pQ = new PriorityQueue();
-pQ.enqueue(39);
-pQ.enqueue(18);
-pQ.enqueue(55);
-pQ.enqueue(41);
-pQ.enqueue(12);
-pQ.enqueue(33);
-pQ.enqueue(27);
-pQ.enqueue(38);
-pQ.extractMax();
-console.log(pQ.values);
+const kClosest = (points,k) =>{
+    let result = [];
+    let queue = new PriorityQueue();
+    for (let point of points) {
+        let distance = Math.floor(Math.sqrt(Math.pow(point[0], 2) + Math.pow(point[1], 2)));
+        queue.enqueue(point, distance);
+    }
 
+    while (k > 0) {
+        result.push(queue.extractMax());
+        k--;
+    }
+    return result;
+
+};
+console.log(kClosest([[3, 3], [5, -1], [-2, 4]], 2));;
+console.log(kClosest( [[1,3],[-2,2]], 1));;
+
+
+/*
+let pQ = new PriorityQueue();
+pQ.enqueue(39,3);
+pQ.enqueue(18, 4);
+pQ.enqueue(55, 1);
+pQ.enqueue(41, 4);
+pQ.enqueue(12, 9);
+pQ.enqueue(33, 11);
+pQ.enqueue(27, 8);
+pQ.enqueue(38, 7);
+console.log(pQ.extractMax());
+console.log(pQ.values);*/
