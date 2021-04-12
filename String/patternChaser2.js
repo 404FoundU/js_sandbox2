d1366faff900ac8dd07341bdb454c5fa49585b9f
upillai <upillai@xpressdocs.com>
2021-04-12 14:18:56 -0500

    added pattern chaser. Longest common substring

--- String/patternChaser2.js
+++ String/patternChaser2.js
@@ -1,14 +1,20 @@
+// https://coderbyte.com/editor/Pattern%20Chaser:JavaScript
 const patternChaser = (str) =>{
-    let result = [];
-    let longest = '';
-    for (let i = 0; i < str.length; i++) {
-        for (let j = i; j < str.length; j++) {
+    let len = str.length;
+    let halfLen = Math.floor(len/2);
 
-
+    for(let i = halfLen; i>1; i--){
+        for(let j=0; j<len -i; j++){
+            let text = str.substr(j,i);
+            let regText = new RegExp(text,'g');
+            let textString = str.match(regText);
+            if(textString.length>1){
+                return 'yes ' + text;
+            }
         }
     }
-    // return longest;
-    return result;
+
+    return 'no null';
 }
 console.log(patternChaser("sskfssbbb9bbb"));
 // console.log(patternChaser("da2kr32a2"));
