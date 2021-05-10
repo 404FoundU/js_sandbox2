ecbaafcfd8c56babfa7afa99d18fedbb303ea019
upillai <upillai@xpressdocs.com>
2021-05-10 11:27:37 -0500

    created graph

---
+++ graph/seatingStudents.js
@@ -0,0 +1,52 @@
+//https://coderbyte.com/information/Seating%20Students
+const seatingStudents = (arr) => {
+    let occupied = arr.splice(1);
+    let k = arr[0];
+    if (k < 2 || k > 24) {
+        return 0;
+    }
+    if (occupied.length === 0) {
+        return k;
+    }
+
+    let adjacencyList = new Map();
+    const addNode = (desk) => {
+        adjacencyList.set(desk, []);
+    }
+    const addEdge = (start, end) => {
+        adjacencyList.get(start).push(end);
+        adjacencyList.get(end).push(start);
+    }
+    // let desks = Array[k].fill()
+    let desks = [];
+    for (let i = 1; i < k+1; i++) {
+        desks[i-1] = i;
+        addNode(i)
+    }
+    let count = 0;
+
+    // create graph
+    // create connection for node pairs
+    for (let i = 0; i < k-1; i = i+2) {
+        addEdge(desks[i], desks[i + 1]);
+        if (!(desks[i] in occupied || desks[i + 1] in occupied)) {
+            count++;
+        }
+    }
+    for (let i = 0; i < k-2; i = i+2) {
+        addEdge(desks[i], desks[i + 2]);
+        if (!(desks[i] in occupied || desks[i + 2] in occupied)) {
+            count++;
+        }
+    }
+    for (let i = 1; i < k-2; i = i+2) {
+        addEdge(desks[i], desks[i + 2]);
+        if (!(desks[i] in occupied || desks[i + 2] in occupied)) {
+            count++;
+        }
+    }
+    return (count + 1) / 2;
+};
+seatingStudents([12, 2, 6, 7, 11]);
+seatingStudents([6,4]);
+console.log(seatingStudents([8,1,8]));
