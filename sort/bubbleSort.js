44a03d24c525a10d14c11d9a1c4fbcd7b2e9dbb4
unni <unnikrishnanrp@outlook.com>
2021-05-20 22:30:57 -0500

    Added bubble

---
+++ sort/bubbleSort.js
@@ -0,0 +1,25 @@
+
+const swap =(arr, i) => {
+    let temp = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = temp;
+}
+const bubbleSort = (arr) => {
+    let count = arr.length;
+    while (count > 0) {
+        let swapped = false;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr, i);
+                swapped = true;
+            }
+        }
+        if (swapped === false) {
+            break;
+        }
+        count--;
+    }
+    return arr;
+
+}
+console.log(bubbleSort([5, 9, 4, 10, 3, 7]));
