ae6bac0b1076ff7c671951dc33c463937d09ace4
upillai <unnikrishnanrp@outlook.com>
2021-07-14 09:56:39 -0500

    kadans algo for maxSum of contiguos arr

--- techniques/window/flipBit.js
+++ techniques/window/flipBit.js
@@ -1,19 +1,35 @@
-const flipBit = (a) => {
-    let arr = [1, 2];
-    let f=0;
-    let count = 0;
-    while (f<a.length) {
-        let bit = a[f];
-        if (bit === 1) {
-            count++;
+//https://practice.geeksforgeeks.org/problems/flip-bits0240/1
+/**
+ * brute force
+ * @param arr
+ * @returns {number}
+ */
+const flipBit = (arr) => {
+    let zeroCount = 0;
+    let maxSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            zeroCount++;
         }
-        f++;
+        let count1 = 0, count0 = 0;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] === 1) {
+                count1++;
+            }
+            else {
+                count0++;
+            }
+            maxSum = Math.max(maxSum, count1 - count0);
+        }
 
     }
-    return count;
+    return zeroCount+maxSum;
 
 };
+/*const flipBit = (arr) => {
 
-let result ;
+}*/
+
 result = flipBit([1, 0, 0, 1, 0]);
+
 console.log(result);
--- techniques/window/largestSumContiguousArray.js
+++ techniques/window/largestSumContiguousArray.js
@@ -1,5 +1,22 @@
 // https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
 //https://www.youtube.com/watch?v=HCL4_bOd3-4
+
+
+//brute force
+/*const largestSum = (arr) => {
+    let maxSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let sum = arr[i];
+        for (let j = 0; j < arr.length; j++) {
+            sum += arr[j];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+    return maxSum
+};*/
+
 // Kadanes algorithm
 const largestSumContiguousArray = (arr) => {
     let sum = 0;
