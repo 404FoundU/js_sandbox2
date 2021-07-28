459d94b82663ef9b18124f2a1113d280e948b114
upillai <unnikrishnanrp@outlook.com>
2021-07-28 11:10:21 -0500

    added console statements for permutations

--- techniques/backtracking/allPermutationOfString.js
+++ techniques/backtracking/allPermutationOfString.js
@@ -1,10 +1,9 @@
 //https://www.youtube.com/watch?v=YK78FU5Ffjw&t=903s
 
-
 const permutation = (s) => {
     const result = [];
     const visited = new Set();
-    const traverse = ( stack, callstack) => {
+    const traverse = ( stack) => {
         if (stack.length === s.length) {
             result.push([...stack]);
             return;
@@ -13,16 +12,43 @@ const permutation = (s) => {
             if (!visited.has(s[i])) {
                 visited.add(s[i])
                 stack.push(s[i]);
-                traverse(stack, callstack+1)
+                traverse(stack)
                 stack.pop();
                 visited.delete(s[i])
             }
 
         }
     };
-    traverse([], 0)
+    traverse([])
     return result;
 }
+/*const permutation = (s) => {
+    const result = [];
+    const visited = new Set();
+    const traverse = ( stack, callstack) => {
+        if (stack.length === s.length) {
+            result.push([...stack]);
+            console.log('result:', result);
+            return;
+        }
+        for (let i = 0; i < s.length; i++) {
+            if (!visited.has(s[i])) {
+                visited.add(s[i])
+                stack.push(s[i]);
+                console.log('stack1:', stack);
+                console.log('callstack1:', callstack);
+                traverse(stack, callstack+1)
+                stack.pop();
+                visited.delete(s[i])
+                console.log('callstackAfterPop:', callstack);
+                console.log('stackAfterPop:', stack);
+            }
+
+        }
+    };
+    traverse([], 0)
+    return result;
+}*/
 let result = permutation("uni");
 // let result = permutation([1,2,3]);
 console.log(result);
--- techniques/backtracking/allSubset.js
+++ techniques/backtracking/allSubset.js
@@ -1,6 +1,25 @@
 //https://leetcode.com/problems/subsets/discuss/?currentPage=1&orderBy=hot&query=&tag=javascript
 //https://www.youtube.com/watch?v=LdtQAYdYLcE
 
+/*const allSubset = (s) => {
+    const result = [];
+    const traverse = (index, stack, callstack) => {
+        //clone
+        result.push([...stack]);
+        console.log('result:', result);
+        for (let i = index; i < s.length; i++) {
+            stack.push(s[i])
+            console.log('stack1:', stack);
+            console.log('callstack1:', callstack);
+            traverse(i + 1, stack, callstack + 1)
+            stack.pop();
+            console.log('callstackAfterPop:', callstack);
+            console.log('stackAfterPop:', stack);
+        }
+    }
+    traverse(0, [], 0);
+    return result
+}*/
 const allSubset = (s) =>{
     const result = [];
     const traverse = (index, stack) => {
