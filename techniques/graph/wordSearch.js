48949557b1c5af6cdaa860daa826a701dcd18c90
upillai <unnikrishnanrp@outlook.com>
2021-08-12 10:15:33 -0500

    Added wordSearch.js

--- techniques/graph/wordSearch.js
+++ techniques/graph/wordSearch.js
@@ -15,27 +15,39 @@ const wordSearch = (word) => {
     let letters = word.split('');
     let letterLength = letters.length
     let visited = new Set();
-    let key = 0 +',' + 0
+    let key = 0 + ',' + 0
     visited.add(key);
-    const queue = [[1,1, 1]];
+    // const queue = [[1, 1, 1, [[1, 1]]]];
+    const queue = [[0, 0, 0, []]];
     const result = [];
     while (queue.length) {
         let point = queue.shift();
-        result.push([point[0], point[1]]);
+        // result.push([point[0], point[1]]);
         for (let dir of directions) {
             let c = point[1] + dir[1];
             let r = point[0] + dir[0];
             let index = point[2];
+            let result = point[3];
+            if (result.length === letterLength) {
+                return result
+            }
             if (c >= 0 && c < column && r >= 0 && r < rows) {
-                key=r+','+c
+                key = r + ',' + c;
                 let letter = grid[r][c];
                 let temp = letters[index];
-                if (letter === letters[index]&& !visited.has(key)) {
-
-                    queue.push([r,c, index+1]);
-                    visited.add(key);
+                if (!visited.has(key)) {
+                    let a = [...result];
+                    if(letter===letters[0]){
+                        index = 0;
+                    }
+                    if (letter === letters[index]) {
+                        a.push([r, c]);
+                        queue.push([r, c, index + 1, a]);
+                        visited.add(key);
+                    } else {
+                        queue.push([r, c, 0, a]);
+                    }
                 }
-
             }
         }
     }
