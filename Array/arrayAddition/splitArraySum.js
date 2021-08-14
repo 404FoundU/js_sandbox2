ccacc7ecda901c7110e5f108c5c12cc166d7f927
unni <unnikrishnanrp@outlook.com>
2021-08-14 17:16:26 -0500

    added split array sum

---
+++ Array/arrayAddition/splitArraySum.js
@@ -0,0 +1,20 @@
+//https://leetcode.com/discuss/interview-experience/1316685/Amazon-OA-SDE1-(new-questions)
+const splitArraySum = (arr) => {
+    let total = 0;
+    for (let val of arr) {
+        total += val;
+    }
+    let count = 0;
+    let leftSum = 0, rightSum = 0;
+    for (let i = 0; i < arr.length-1; i++) {
+        leftSum += arr[i];
+        rightSum = total - leftSum;
+        if (leftSum > rightSum) {
+            count++;
+        }
+    }
+    return count;
+};
+
+let a = [10, 4, -8, 7];
+console.log(splitArraySum(a));
